68a23350b8586ebac0b1e9580cfd995b31efd4e0
Maurits Bos <mb.brains@gmail.com>
2023-03-21 00:06:30 +0100

    Bijnaa klaar!

--- contracts/cw-staking/src/providers/osmosis.rs
+++ contracts/cw-staking/src/providers/osmosis.rs
@@ -1,6 +1,10 @@
 use cosmwasm_std::Addr;
+use cw_utils::Duration;
+use osmosis_std::{
+    shim::Duration as OsmosisDuration, types::osmosis::superfluid::SuperfluidAssetType,
+};
 
-use crate::traits::identify::Identify;
+use crate::{error::StakingError, traits::identify::Identify};
 
 pub const OSMOSIS: &str = "osmosis";
 
@@ -8,9 +12,10 @@ pub const OSMOSIS: &str = "osmosis";
 pub struct Osmosis {
     pub local_proxy_addr: Option<Addr>,
     pub pool_id: Option<u64>,
-    pub pool_addr: Option<Addr>,
-    pub sf_lock_id: Option<u64>,
+    pub pool_denom: Option<String>,
+    pub lock_id: Option<u64>,
     pub validator_addr: Option<Addr>,
+    pub is_superfluid: Option<bool>,
 }
 
 impl Identify for Osmosis {
@@ -18,15 +23,22 @@ impl Identify for Osmosis {
         OSMOSIS
     }
 }
+
 #[cfg(feature = "osmosis")]
 pub mod fns {
+    use crate::msg::Claim;
     use crate::{error::StakingError, msg::StakingInfoResponse, CwStakingAdapter};
 
     use super::*;
     const FORTEEN_DAYS: i64 = 60 * 60 * 24 * 14;
-    use abstract_core::objects::LpToken;
-    use abstract_sdk::Resolve;
-    use cosmwasm_std::Env;
+    use cosmwasm_std::{CosmosMsg, Deps, Querier, StdError, StdResult, Timestamp, Uint256};
+    use cosmwasm_std::{Decimal, Decimal256, Env, Uint128};
+    use cw_asset::{Asset, AssetInfoBase};
+    use cw_utils::{Duration, Expiration};
+    use osmosis_std::types::osmosis::lockup::{
+        AccountLockedLongerDurationDenomRequest, AccountUnlockingCoinsRequest, PeriodLock,
+    };
+    use osmosis_std::types::osmosis::superfluid::AssetTypeRequest;
     use osmosis_std::{
         shim::Duration as OsmosisDuration,
         types::osmosis::gamm::v1beta1::{
@@ -36,7 +48,10 @@ pub mod fns {
             gamm::v1beta1::QueryPoolResponse,
             superfluid::{MsgLockAndSuperfluidDelegate, MsgSuperfluidUndelegateAndUnbondLock},
         },
-        types::{cosmos::base::query, osmosis::lockup::MsgLockTokens},
+        types::{
+            cosmos::base::query,
+            osmosis::lockup::{MsgBeginUnlocking, MsgLockTokens},
+        },
         types::{
             cosmos::base::v1beta1::Coin as OsmoCoin,
             osmosis::gamm::v1beta1::{Pool, QueryPoolRequest},
@@ -45,6 +60,10 @@ pub mod fns {
 
     /// Osmosis app-chain dex implementation
     impl CwStakingAdapter for Osmosis {
+        /// Fetching data for osmosis staking involves:
+        /// 1. Fetch the pool address from ANS (has pool id)
+        /// 2. Fetch the lock_id from osmosis (can be done with pool address, address and duration)
+        /// 3.
         fn fetch_data(
             &mut self,
             deps: cosmwasm_std::Deps,
@@ -54,39 +73,44 @@ pub mod fns {
         ) -> abstract_sdk::AbstractSdkResult<()> {
             let provider_staking_contract_entry = self.staking_entry(&staking_asset);
 
-            // self.local_proxy_addr = Some("something_else");
-
-            let pool_addr =
+            // This is going to be the gamm pool address
+            let gamm_pool_address =
                 ans_host.query_contract(&deps.querier, &provider_staking_contract_entry)?;
-            // TODO: How is the staking address the local proxy address?
+
+            // This is going to be the gamm pool id
+            let gamm_pool_id = gamm_pool_address
+                .to_string()
+                .split("/")
+                .last()
+                .unwrap()
+                .parse::<u64>()
+                .map_err(|_| StdError::generic_err("Unable to parse gamm pool id"))?;
+
+            self.local_proxy_addr = Some(Addr::unchecked(
+                "This is going to be the local proxy address",
+            ));
 
             // TODO: this is the validator address
             self.validator_addr = Some(Addr::unchecked(
                 "osmovaloper1v9w0j3x7q5yqy0y3q3x6y2z7xqz3zq5q9zq3zq",
             ));
 
-            // TODO: this is the superfluid lock id which 'i think' is generated at first lock
-            // We need a way to initialize that lock id and store it...
-            self.sf_lock_id = Some(10);
+            // TODO: this is the superfluid lock id
+            self.pool_denom = Some(gamm_pool_address.to_string());
+            self.pool_id = Some(gamm_pool_id);
 
-            // TODO: this is the gamm pool address/number. Im not sure how this will be read/stored
-            self.pool_addr = Some(pool_addr); // Some(Addr::unchecked("gamm/pool/69"));
-                                              // TODO: this is the gamm pool id. Im not sure how this will be read/stored
-            self.pool_id = Some(69);
+            // superfluid asset type 0 is for non-superfluid pools and 1 is for superfluid pools
+            // see docs: https://github.com/osmosis-labs/osmosis/tree/main/x/superfluid#assettype
+            self.is_superfluid =
+                Some(self.query_is_superfluid(&deps.querier, self.pool_denom.clone().unwrap())?);
+
             Ok(())
-        }
-
-        /// **STAKING FLOW OF OSMOSIS**
-        /// When staking LP/GAMM tokens, there are 2 options: lock and superfluid delegate or lock tokens.
-        /// for some pools, superfluid delegate is not available, so we need to use lock tokens.
-        ///
-        /// ## *non-superfluid pools*
         /// In the case of *non-superfluid pools*, the flow will be as follows:
         /// -- first time locking tokens --
         /// 1. lock tokens: *MsgLockTokens*
         ///     parameters:
         ///     * duration
-        ///     * owner
+        ///     * sender
         ///     * coins
         ///
         /// -- subsequent staking --
@@ -94,10 +118,10 @@ pub mod fns {
         ///    parameters: same
         ///
         /// -- unstaking --
-        /// 1. unlock tokens: *MsgBeginUnlockPeriodLock*
+        /// 1. unlock tokens: *MsgBeginUnlocking* ( begin-unlock-period-lock)
         ///    parameters:
         ///   * lock_id
-        ///   * owner
+        ///   * sender
         ///   * coins  
         ///
         /// ## *superfluid pools*
@@ -113,14 +137,14 @@ pub mod fns {
         /// 2nd time lock -> lock tokens: *MsgLockTokens*
         ///   parameters:
         ///     * duration
-        ///     * owner
+        ///     * sender
         ///     * coins
         ///
         /// -- unstaking (v15) --
         /// -> unlock tokens: *MsgSuperfluidUndelegateAndUnbondLock* (v15)
         ///   parameters:
         ///    * lock_id
-        ///    * owner
+        ///    * sender
         ///    * coins
         ///
         /// -- unstaking (v14) --
@@ -133,7 +157,7 @@ pub mod fns {
         ///    2). *MsgSuperFluidUnbondLock*
         ///   parameters:
         ///   * lock_id
-        ///   * owner
+        ///   * sender
         ///
         ///
         fn stake(
@@ -142,64 +166,82 @@ pub mod fns {
             amount: cosmwasm_std::Uint128,
             unbonding_period: Option<cw_utils::Duration>,
         ) -> Result<Vec<cosmwasm_std::CosmosMsg>, StakingError> {
-            // Query lock id from the proxy address (local proxy address)
-            let proxy_address = self.local_proxy_addr.as_ref().unwrap();
-            let validator = self.validator_addr.as_ref().unwrap();
-
-            deps.querier.query_delegation(proxy_address, validator);
-            deps.querier.query_all_delegations(proxy_address);
+            let proxy_addr = self.local_proxy_addr.as_ref().unwrap();
+            let duration = unwrap_unbond(self, unbonding_period)?;
+            let is_superfluid = self.is_superfluid.unwrap();
 
             let coin = OsmoCoin {
                 // NOTE: This shold be the gamm token address
-                denom: self.pool_addr.as_ref().unwrap().to_string(),
+                denom: self.pool_denom.as_ref().unwrap().to_string(),
                 amount: amount.to_string(),
             };
 
-            // let msg: CosmosMsg = MsgLockAndSuperfluidDelegate {
-            //     sender: proxy_addr.to_string(),
-            //     coins: vec![coin],
-            //     val_addr: self.validator_addr.as_ref().unwrap().to_string(),
-            // }
-            // .into();
-            //
-            // From my understanding the lock and superfluid delegate is the same as the lock tokens,
-            // but when i use osmosis frontend it uses the lock tokens.
-            let msg: CosmosMsg = MsgLockTokens {
-                duration: Some(OsmosisDuration {
-                    seconds: FORTEEN_DAYS,
-                    nanos: 0,
-                }),
-                owner: self.local_proxy_addr.as_ref().unwrap().to_string(),
-                coins: vec![coin],
-            }
-            .into();
+            let lock_id = self.query_lock_id(
+                duration.clone(),
+                self.local_proxy_addr.as_ref().unwrap(),
+                &deps.querier,
+            )?;
+
+            let msg: CosmosMsg = match (lock_id, is_superfluid) {
+                // No lock id but superfluid => first time superfluid staking
+                (None, true) => MsgLockAndSuperfluidDelegate {
+                    sender: proxy_addr.to_string(),
+                    coins: vec![coin],
+                    val_addr: self.validator_addr.as_ref().unwrap().to_string(),
+                }
+                .into(),
+                // all other cases [(lock_id, is_superfluid) => (Some, true), (Some, false), (None, false))]
+                _ => MsgLockTokens {
+                    duration: Some(duration),
+                    owner: self.local_proxy_addr.as_ref().unwrap().to_string(),
+                    coins: vec![coin],
+                }
+                .into(),
+            };
 
             Ok(vec![msg])
         }
 
+        // TODO: workout edgecase of not superfluid staking pools
         fn unstake(
             &self,
             deps: Deps,
             amount: cosmwasm_std::Uint128,
             unbonding_period: Option<cw_utils::Duration>,
         ) -> Result<Vec<cosmwasm_std::CosmosMsg>, StakingError> {
+            let duration = unwrap_unbond(self, unbonding_period)?;
+            let proxy_addr = self.local_proxy_addr.as_ref().unwrap();
+            let is_superfluid = self.is_superfluid.unwrap();
+
+            let lock_id = self
+                .query_lock_id(duration, proxy_addr, &deps.querier)?
+                .unwrap();
+
             let coin = OsmoCoin {
                 // NOTE: This shold be the gamm token address ??
-                denom: self.pool_addr.as_ref().unwrap().to_string(),
+                denom: self.pool_denom.as_ref().unwrap().to_string(),
                 amount: amount.to_string(),
             };
-            // Note this should be the osmo address of the sender
-            let msg: CosmosMsg = MsgSuperfluidUndelegateAndUnbondLock {
-                sender: self.local_proxy_addr.as_ref().unwrap().to_string(),
-                lock_id: self.sf_lock_id.unwrap(),
-                coin: Some(coin),
-            }
-            .into();
+
+            let msg: CosmosMsg = match is_superfluid {
+                true => MsgSuperfluidUndelegateAndUnbondLock {
+                    sender: proxy_addr.to_string(),
+                    lock_id,
+                    coin: Some(coin),
+                }
+                .into(),
+                false => MsgBeginUnlocking {
+                    id: lock_id,
+                    owner: proxy_addr.to_string(),
+                    coins: vec![coin],
+                }
+                .into(),
+            };
 
             Ok(vec![msg])
         }
 
-        fn claim(&self, deps: Deps) -> Result<Vec<cosmwasm_std::CosmosMsg>, StakingError> {
+        fn claim(&self, _deps: Deps) -> Result<Vec<cosmwasm_std::CosmosMsg>, StakingError> {
             // Check: Im not sure if this is correct
             // Claim is not nesseccary for osmosis
             Ok(vec![])
@@ -226,9 +268,9 @@ pub mod fns {
 
             let res = StakingInfoResponse {
                 staking_token: AssetInfoBase::Cw20(Addr::unchecked(
-                    self.pool_addr.as_ref().unwrap().to_string(),
+                    self.pool_denom.as_ref().unwrap().to_string(),
                 )),
-                staking_contract_address: self.pool_addr.clone().unwrap(),
+                staking_contract_address: Addr::unchecked(self.pool_denom.as_ref().unwrap()),
                 unbonding_periods: Some(vec![]),
                 max_claims: None,
             };
@@ -242,7 +284,17 @@ pub mod fns {
             staker: Addr,
             unbonding_period: Option<cw_utils::Duration>,
         ) -> crate::contract::CwStakingResult<crate::msg::StakeResponse> {
-            todo!()
+            let duration = unwrap_unbond(self, unbonding_period)?;
+            let lock = self.query_lock(duration, &staker, querier)?;
+
+            let amount = match lock {
+                Some(lock) => lock.coins.iter().fold(Uint128::zero(), |acc, coin| {
+                    acc + coin.amount.parse::<Uint128>().unwrap()
+                }),
+                None => Uint128::zero(),
+            };
+
+            Ok(crate::msg::StakeResponse { amount })
         }
 
         fn query_unbonding(
@@ -250,7 +302,23 @@ pub mod fns {
             querier: &cosmwasm_std::QuerierWrapper,
             staker: Addr,
         ) -> crate::contract::CwStakingResult<crate::msg::UnbondingResponse> {
-            todo!()
+            // NOTE: THIS IS NOT CORRECT -> We dont have unbonding period here, so we have to return the sum of all locks
+            let unbonding = unwrap_unbond(self, Some(Duration::Time(0)))?;
+
+            let locks = self.query_locks(unbonding, &staker, querier)?;
+            let response = crate::msg::UnbondingResponse {
+                claims: locks
+                    .into_iter()
+                    .map(|lock| Claim {
+                        amount: lock.coins.first().unwrap().amount.parse().unwrap(),
+                        claimable_at: Expiration::AtTime(
+                            Timestamp::from_seconds(lock.end_time.as_ref().unwrap().seconds as u64)
+                                .plus_nanos(lock.end_time.unwrap().nanos as u64),
+                        ),
+                    })
+                    .collect::<Vec<Claim>>(),
+            };
+            Ok(response)
         }
 
         fn query_reward_tokens(
@@ -260,46 +328,23 @@ pub mod fns {
             todo!()
         }
 
-        fn claim_rewards(&self, deps: Deps) -> Result<Vec<cosmwasm_std::CosmosMsg>, StakingError> {
-            Ok(vec![])
-        }
-
-        // fn query_pool_data(
-        //     &self,
-        //     querier: &cosmwasm_std::QuerierWrapper,
-        //     pool_id: u64,
-        // ) -> StdResult<Pool> {
-        //     let res = QueryPoolRequest { pool_id }.query(&querier).unwrap();
-
-        //     let pool = Pool::try_from(res.pool.unwrap()).unwrap();
-        //     Ok(pool)
-        // }
-
-        fn query_info(
+    impl Osmosis {
+        fn query_lock_id(
             &self,
+            duration: OsmosisDuration,
+            owner: &Addr,
             querier: &cosmwasm_std::QuerierWrapper,
-        ) -> crate::contract::CwStakingResult<crate::msg::StakingInfoResponse> {
-            let pool = self
-                .query_pool_data(querier, self.pool_id.unwrap())
-                .unwrap();
+        ) -> Result<Option<u64>, StakingError> {
+            // This query returns all the locks that are equal or longer than the duration
+            // there is no query that returns the lock with the exact duration AND
+            // osmosis docs do not specify the order if there are multiple locks
+            // so we have to sort based on duration and take the first, which should be the one that equals the duration
+            let lock = self.query_lock(duration, owner, querier)?;
 
-            let res = StakingInfoResponse {
-                staking_token: AssetInfoBase::Cw20(Addr::unchecked(
-                    self.pool_addr.as_ref().unwrap().to_string(),
-                )),
-                staking_contract_address: self.pool_addr.clone().unwrap(),
-                unbonding_periods: Some(vec![]),
-                max_claims: None,
-            };
-
-            Ok(res)
-        }
-
-        fn query_pool_data(deps: Deps, pool_id: u64) -> StdResult<Pool> {
-            let res = QueryPoolRequest { pool_id }.query(&deps.querier).unwrap();
-
-            let pool = Pool::try_from(res.pool.unwrap()).unwrap();
-            Ok(pool)
+            match lock {
+                Some(lock) => Ok(Some(lock.id)),
+                None => Ok(None),
+            }
         }
 
         fn compute_osmo_share_out_amount(
@@ -363,5 +408,81 @@ pub mod fns {
 
             Ok(())
         }
+
+        fn query_pool_data(
+            &self,
+            querier: &cosmwasm_std::QuerierWrapper,
+            pool_id: u64,
+        ) -> StdResult<Pool> {
+            let res = QueryPoolRequest { pool_id }.query(&querier).unwrap();
+
+            let pool = Pool::try_from(res.pool.unwrap()).unwrap();
+            Ok(pool)
+        }
+
+        fn query_is_superfluid(
+            &self,
+            querier: &cosmwasm_std::QuerierWrapper,
+            denom: String,
+        ) -> StdResult<bool> {
+            let asset_type = AssetTypeRequest { denom }.query(&querier)?.asset_type;
+
+            match asset_type {
+                0 => Ok(false),
+                1 => Ok(true),
+                _ => Err(StdError::generic_err("invalid asset type")),
+            }
+        }
+
+        fn query_lock(
+            &self,
+            duration: OsmosisDuration,
+            staker: &Addr,
+            querier: &cosmwasm_std::QuerierWrapper,
+        ) -> StdResult<Option<PeriodLock>> {
+            let locks = self.query_locks(duration, staker, querier)?;
+
+            Ok(locks.first().cloned())
+        }
+
+        fn query_locks(
+            &self,
+            duration: OsmosisDuration,
+            staker: &Addr,
+            querier: &cosmwasm_std::QuerierWrapper,
+        ) -> Result<Vec<PeriodLock>, StdError> {
+            let mut locks = AccountLockedLongerDurationDenomRequest {
+                duration: Some(duration),
+                denom: self.pool_denom.as_ref().unwrap().to_string(),
+                owner: staker.to_string(),
+            }
+            .query(&querier)?
+            .locks;
+            locks.sort_by(|a, b| {
+                a.duration
+                    .as_ref()
+                    .unwrap()
+                    .seconds
+                    .cmp(&b.duration.as_ref().unwrap().seconds)
+            });
+            Ok(locks)
+        }
     }
+}
+
+fn unwrap_unbond(
+    dex: &Osmosis,
+    unbonding_period: Option<Duration>,
+) -> Result<OsmosisDuration, StakingError> {
+    let Some(Duration::Time(unbonding_period)) = unbonding_period else {
+        if unbonding_period.is_none() {
+            return Err(StakingError::UnbondingPeriodNotSet(dex.name().to_owned()));
+        } else {
+            return Err(StakingError::UnbondingPeriodNotSupported("height".to_owned(), dex.name().to_owned()));
+        }
+    };
+    Ok(OsmosisDuration {
+        seconds: unbonding_period as i64,
+        nanos: 0,
+    })
 }
