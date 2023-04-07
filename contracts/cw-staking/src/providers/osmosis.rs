8baa85c7bc1492806d0346b89e98a3254a05a4ed
Maurits Bos <mb.brains@gmail.com>
2023-04-07 16:35:08 +0200

    processed suggestions from @stackman27

--- contracts/cw-staking/src/providers/osmosis.rs
+++ contracts/cw-staking/src/providers/osmosis.rs
@@ -11,7 +11,6 @@ pub struct Osmosis {
     pub local_proxy_addr: Option<Addr>,
     pub pool_id: Option<u64>,
     pub pool_denom: Option<String>,
-    pub lock_id: Option<u64>,
     pub validator_addr: Option<Addr>,
     pub is_superfluid: Option<bool>,
 }
@@ -24,31 +23,27 @@ impl Identify for Osmosis {
 
 #[cfg(feature = "osmosis")]
 pub mod fns {
+    use std::str::FromStr;
+
     use crate::msg::Claim;
     use crate::{error::StakingError, msg::StakingInfoResponse, CwStakingAdapter};
 
     use super::*;
-    const FORTEEN_DAYS: i64 = 60 * 60 * 24 * 14;
-    use cosmwasm_std::{CosmosMsg, Deps, Querier, StdError, StdResult, Timestamp, Uint256};
-    use cosmwasm_std::{Decimal, Decimal256, Env, Uint128};
-    use cw_asset::{Asset, AssetInfo, AssetInfoBase};
+    use cosmwasm_std::{CosmosMsg, Deps, StdError, StdResult, Timestamp};
+    use cosmwasm_std::{Decimal, Env, Uint128};
+    use cw_asset::{AssetInfo, AssetInfoBase};
     use cw_utils::{Duration, Expiration};
-    use osmosis_std::types::osmosis::lockup::{
-        AccountLockedLongerDurationDenomRequest, PeriodLock,
-    };
+    use osmosis_std::types::osmosis::lockup::{AccountLockedDurationRequest, PeriodLock};
     use osmosis_std::types::osmosis::poolincentives::v1beta1::{
-        QueryExternalIncentiveGaugesRequest, QueryIncentivizedPoolsRequest,
+        QueryExternalIncentiveGaugesRequest, QueryGaugeIdsRequest,
     };
-    use osmosis_std::types::osmosis::superfluid::AssetTypeRequest;
+    use osmosis_std::types::osmosis::superfluid::QueryUnpoolWhitelistRequest;
     use osmosis_std::{
         shim::Duration as OsmosisDuration,
+        types::cosmos::base::v1beta1::Coin as OsmoCoin,
         types::osmosis::lockup::{MsgBeginUnlocking, MsgLockTokens},
         types::osmosis::superfluid::{
             MsgLockAndSuperfluidDelegate, MsgSuperfluidUndelegateAndUnbondLock,
-        },
-        types::{
-            cosmos::base::v1beta1::Coin as OsmoCoin,
-            osmosis::gamm::v1beta1::{Pool, QueryPoolRequest},
         },
     };
 
@@ -85,10 +80,8 @@ pub mod fns {
                 "osmovaloper1v9w0j3x7q5yqy0y3q3x6y2z7xqz3zq5q9zq3zq",
             ));
 
-            // TODO: this is the superfluid lock id
             self.pool_denom = Some(gamm_pool_address.to_string());
             self.pool_id = Some(gamm_pool_id);
-
             self.is_superfluid =
                 Some(self.query_is_superfluid(&deps.querier, self.pool_denom.clone().unwrap())?);
 
@@ -159,15 +152,14 @@ pub mod fns {
             let is_superfluid = self.is_superfluid.unwrap();
 
             let coin = OsmoCoin {
-                // NOTE: This shold be the gamm token address
                 denom: self.pool_denom.as_ref().unwrap().to_string(),
                 amount: amount.to_string(),
             };
 
             let lock_id = self.query_lock_id(
+                &deps.querier,
                 duration.clone(),
                 self.local_proxy_addr.as_ref().unwrap(),
-                &deps.querier,
             )?;
 
             let msg: CosmosMsg = match (lock_id, is_superfluid) {
@@ -201,11 +193,10 @@ pub mod fns {
             let is_superfluid = self.is_superfluid.unwrap();
 
             let lock_id = self
-                .query_lock_id(duration, proxy_addr, &deps.querier)?
+                .query_lock_id(&deps.querier, duration, proxy_addr)?
                 .unwrap();
 
             let coin = OsmoCoin {
-                // NOTE: This shold be the gamm token address ??
                 denom: self.pool_denom.as_ref().unwrap().to_string(),
                 amount: amount.to_string(),
             };
@@ -238,15 +229,17 @@ pub mod fns {
             &self,
             querier: &cosmwasm_std::QuerierWrapper,
         ) -> crate::contract::CwStakingResult<crate::msg::StakingInfoResponse> {
-            // NOTE: This seems to only return the longest duration atm
-            let lockable_durations = QueryIncentivizedPoolsRequest {}
-                .query(querier)
-                .unwrap()
-                .incentivized_pools
-                .into_iter()
-                .filter(|p| p.pool_id == self.pool_id.unwrap())
-                .map(|d| Duration::Time(d.lockable_duration.unwrap().seconds as u64))
-                .collect::<Vec<_>>();
+            let lockable_durations = QueryGaugeIdsRequest {
+                pool_id: self.pool_id.unwrap(),
+            }
+            .query(querier)
+            .unwrap()
+            .gauge_ids_with_duration
+            .into_iter()
+            // TODO: This is a hack to filter out gauges with 0% incentive
+            .filter(|g| Decimal::from_str(&g.gauge_incentive_percentage).unwrap() > Decimal::zero())
+            .map(|g| Duration::Time(g.duration.unwrap().seconds as u64))
+            .collect::<Vec<_>>();
 
             let res = StakingInfoResponse {
                 staking_token: AssetInfoBase::Cw20(Addr::unchecked(
@@ -267,17 +260,16 @@ pub mod fns {
             unbonding_period: Option<cw_utils::Duration>,
         ) -> crate::contract::CwStakingResult<crate::msg::StakeResponse> {
             let duration = unwrap_unbond(self, unbonding_period)?;
-            let lock = self.query_lock(duration, &staker, querier)?;
-
-            // Assume that the lock is for only one denom
-            let amount = match lock {
-                Some(lock) => lock.coins.iter().fold(Uint128::zero(), |acc, coin| {
-                    acc + coin.amount.parse::<Uint128>().unwrap()
-                }),
-                None => Uint128::zero(),
-            };
-
-            Ok(crate::msg::StakeResponse { amount })
+            let lock = self.query_lock(querier, duration, &staker, Some(false))?;
+            if let Some(lock) = lock {
+                return Ok(crate::msg::StakeResponse {
+                    amount: lock.coins.first().unwrap().amount.parse().unwrap(),
+                });
+            } else {
+                return Ok(crate::msg::StakeResponse {
+                    amount: Uint128::zero(),
+                });
+            }
         }
 
         fn query_unbonding(
@@ -285,33 +277,30 @@ pub mod fns {
             querier: &cosmwasm_std::QuerierWrapper,
             staker: Addr,
         ) -> crate::contract::CwStakingResult<crate::msg::UnbondingResponse> {
-            // NOTE: THIS IS NOT CORRECT -> We dont have unbonding period available in this func here,
-            // so we have to return the sum of all locks
-            // Possible solution: take the unbonding period from the lock in fetch_data()
             let unbonding = unwrap_unbond(self, Some(Duration::Time(0)))?;
+            let locks = self.query_locks(querier, unbonding, &staker, Some(true))?;
+            let claims = locks
+                .into_iter()
+                .map(|lock| Claim {
+                    amount: lock.coins.first().unwrap().amount.parse().unwrap(),
+                    claimable_at: Expiration::AtTime(
+                        Timestamp::from_seconds(lock.end_time.as_ref().unwrap().seconds as u64)
+                            .plus_nanos(lock.end_time.unwrap().nanos as u64),
+                    ),
+                })
+                .collect::<Vec<Claim>>();
 
-            let locks = self.query_locks(unbonding, &staker, querier)?;
-            let response = crate::msg::UnbondingResponse {
-                claims: locks
-                    .into_iter()
-                    .map(|lock| Claim {
-                        amount: lock.coins.first().unwrap().amount.parse().unwrap(),
-                        claimable_at: Expiration::AtTime(
-                            Timestamp::from_seconds(lock.end_time.as_ref().unwrap().seconds as u64)
-                                .plus_nanos(lock.end_time.unwrap().nanos as u64),
-                        ),
-                    })
-                    .collect::<Vec<Claim>>(),
-            };
+            let response = crate::msg::UnbondingResponse { claims };
             Ok(response)
         }
 
+        // FIXME: This is the only query we dont have a clear answer for yet
         fn query_reward_tokens(
             &self,
             querier: &cosmwasm_std::QuerierWrapper,
         ) -> crate::contract::CwStakingResult<crate::msg::RewardTokensResponse> {
             // NOTE: This query is super inefficient but i dont know how to do it better
-            let external_reward_gauges = QueryExternalIncentiveGaugesRequest {}
+            let reward_tokens = QueryExternalIncentiveGaugesRequest {}
                 .query(querier)
                 .unwrap()
                 .data
@@ -324,14 +313,9 @@ pub mod fns {
                         .denom
                         .eq(self.pool_denom.as_ref().unwrap())
                 })
-                .collect::<Vec<_>>();
-
-            let reward_tokens = external_reward_gauges
-                .into_iter()
-                .map(|g| g.coins)
-                .flatten()
+                .flat_map(|g| g.coins)
                 .map(|coin| AssetInfo::Native(coin.denom))
-                .collect::<Vec<AssetInfo>>();
+                .collect::<Vec<_>>();
 
             Ok(crate::msg::RewardTokensResponse {
                 tokens: reward_tokens,
@@ -342,11 +326,11 @@ pub mod fns {
         /// queries the lock_id for the given duration and account(staker)
         fn query_lock_id(
             &self,
+            querier: &cosmwasm_std::QuerierWrapper,
             duration: OsmosisDuration,
             staker: &Addr,
-            querier: &cosmwasm_std::QuerierWrapper,
         ) -> Result<Option<u64>, StakingError> {
-            let lock = self.query_lock(duration, staker, querier)?;
+            let lock = self.query_lock(querier, duration, staker, Some(false))?;
 
             match lock {
                 Some(lock) => Ok(Some(lock.id)),
@@ -354,31 +338,20 @@ pub mod fns {
             }
         }
 
-        /// queries weahter the given denom is a superfluid asset or not
-        /// superfluid asset type 0 is for non-superfluid pools and 1 is for superfluid pools
-        /// see docs: https://github.com/osmosis-labs/osmosis/tree/main/x/superfluid#assettype
-        fn query_is_superfluid(
-            &self,
-            querier: &cosmwasm_std::QuerierWrapper,
-            denom: String,
-        ) -> StdResult<bool> {
-            let asset_type = AssetTypeRequest { denom }.query(&querier)?.asset_type;
-
-            match asset_type {
-                0 => Ok(false),
-                1 => Ok(true),
-                _ => Err(StdError::generic_err("invalid asset type")),
-            }
-        }
-
         /// Query the lock with the exact duration.
         fn query_lock(
             &self,
+            querier: &cosmwasm_std::QuerierWrapper,
             duration: OsmosisDuration,
             staker: &Addr,
-            querier: &cosmwasm_std::QuerierWrapper,
+            is_unbonding: Option<bool>,
         ) -> StdResult<Option<PeriodLock>> {
-            let locks = self.query_locks(duration, staker, querier)?;
+            let locks = self.query_locks(querier, duration, staker, is_unbonding)?;
+            // so we have to filter out the synthetic locks
+            let locks = locks
+                .into_iter()
+                .filter(|p| p.end_time.is_none())
+                .collect::<Vec<_>>();
             Ok(locks.first().cloned())
         }
 
@@ -386,42 +359,58 @@ pub mod fns {
         /// they are sorted ascending by duration.
         fn query_locks(
             &self,
+            querier: &cosmwasm_std::QuerierWrapper,
             duration: OsmosisDuration,
             staker: &Addr,
-            querier: &cosmwasm_std::QuerierWrapper,
+            is_unbonding: Option<bool>,
         ) -> Result<Vec<PeriodLock>, StdError> {
-            // This query returns all the locks that are equal or longer than the duration
-            // there is no query that returns the lock with the exact duration AND
-            // osmosis docs do not specify the order if there are multiple locks
-            // so we have to sort based on duration and take the first, which should be the one that equals the duration
-            let mut locks = AccountLockedLongerDurationDenomRequest {
+            // query all the locks for the given staker and duration and then
+            // filter out the ones that are not for the pool denom
+            // this query returns the lock with the current staked, but also all the synthetic locks,
+            // which are locks that are currently unbonding.
+            let mut locks = AccountLockedDurationRequest {
                 duration: Some(duration),
-                denom: self.pool_denom.as_ref().unwrap().to_string(),
                 owner: staker.to_string(),
             }
-            .query(&querier)?
+            .query(querier)?
             .locks;
 
-            locks.sort_by(|a, b| {
-                a.duration
-                    .as_ref()
-                    .unwrap()
-                    .seconds
-                    .cmp(&b.duration.as_ref().unwrap().seconds)
-            });
+            if let Some(denom) = self.pool_denom.as_ref() {
+                locks = locks
+                    .into_iter()
+                    .filter(|p| p.coins.first().unwrap().denom.eq(denom))
+                    .collect::<Vec<_>>();
+            }
+
+            if let Some(is_unbonding) = is_unbonding {
+                locks = locks
+                    .into_iter()
+                    .filter(|p| p.end_time.is_some() == is_unbonding)
+                    .collect::<Vec<_>>();
+            }
+
             Ok(locks)
         }
 
-        /// queries the pool data for the given pool id
-        fn query_pool_data(
+        /// queries wether the given denom is a superfluid asset or not
+        fn query_is_superfluid(
             &self,
             querier: &cosmwasm_std::QuerierWrapper,
-            pool_id: u64,
-        ) -> StdResult<Pool> {
-            let res = QueryPoolRequest { pool_id }.query(&querier).unwrap();
-
-            let pool = Pool::try_from(res.pool.unwrap()).unwrap();
-            Ok(pool)
+            denom: String,
+        ) -> StdResult<bool> {
+            let res = QueryUnpoolWhitelistRequest {}.query(querier)?.pool_ids;
+            if res.contains(
+                &denom
+                    .split('/')
+                    .last()
+                    .unwrap()
+                    .parse::<u64>()
+                    .map_err(|_| StdError::generic_err("Unable to parse gamm pool id"))?,
+            ) {
+                Ok(true)
+            } else {
+                Ok(false)
+            }
         }
     }
 }
